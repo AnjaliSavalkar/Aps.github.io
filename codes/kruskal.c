04e830b3306d21af7f286175a59b8949f65aaac5
AnjaliSavalkar <savalkaranjali6@gmail.com>
2025-05-29 20:01:03 +0530

    Update kruskal.c

--- codes/kruskal.c
+++ codes/kruskal.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure to represent a weighted edge
 typedef struct {
     int u, v, weight;
 } Edge;
 
-// Disjoint-set (union-find) data structure
 int parent[100], rank[100];
 
-// Find function with path compression
 int find(int i) {
     if (parent[i] == i)
         return i;
     return parent[i] = find(parent[i]);
 }
 
-// Union function to combine two sets
 void unionSets(int u, int v) {
     int root_u = find(u);
     int root_v = find(v);
     if (root_u != root_v) {
-        // Union by rank
         if (rank[root_u] > rank[root_v]) {
             parent[root_v] = root_u;
         } else if (rank[root_u] < rank[root_v]) {
@@ -33,20 +28,16 @@ void unionSets(int u, int v) {
     }
 }
 
-// Function to compare two edges by their weight (for sorting)
 int compareEdges(const void *a, const void *b) {
     return ((Edge*)a)->weight - ((Edge*)b)->weight;
 }
 
-// Kruskal's algorithm to find the MST
 void kruskal(Edge edges[], int numEdges, int numVertices) {
-    // Initialize disjoint-set
     for (int i = 0; i < numVertices; i++) {
         parent[i] = i;
         rank[i] = 0;
     }
 
-    // Sort edges by weight
     qsort(edges, numEdges, sizeof(Edge), compareEdges);
 
     int mstWeight = 0;
@@ -54,24 +45,19 @@ void kruskal(Edge edges[], int numEdges, int numVertices) {
     for (int i = 0; i < numEdges; i++) {
         int u = edges[i].u;
         int v = edges[i].v;
-
-        // If u and v are in different sets, add the edge to the MST
         if (find(u) != find(v)) {
             printf("%d - %d : %d\n", u, v, edges[i].weight);
             mstWeight += edges[i].weight;
             unionSets(u, v);
         }
     }
-
     printf("Total weight of MST: %d\n", mstWeight);
 }
 
 int main() {
-    // Number of vertices (warehouses) and edges (transportation routes)
-    int numVertices = 5; // Example: 5 warehouses
-    int numEdges = 7;    // Example: 7 transportation routes
+    int numVertices = 5;
+    int numEdges = 7;
 
-    // List of edges (u, v, weight)
     Edge edges[] = {
         {0, 1, 2},
         {0, 3, 6},
@@ -82,7 +68,6 @@ int main() {
         {3, 4, 9}
     };
 
-    // Run Kruskal’s Algorithm
     kruskal(edges, numEdges, numVertices);
 
     return 0;
