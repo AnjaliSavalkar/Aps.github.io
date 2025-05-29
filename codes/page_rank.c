962044cd80ad72da0bf181974d016f4f6c80c56c
AnjaliSavalkar <savalkaranjali6@gmail.com>
2025-05-29 19:39:36 +0530

    Update page_rank.c

--- codes/page_rank.c
+++ codes/page_rank.c
@@ -2,30 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define N 4 // Number of products (nodes)
+#define N 4
 #define MAX_ITER 100
-#define DAMPING 0.85 // Damping factor for PageRank
+#define DAMPING 0.85
 
-// Function to perform PageRank on the product graph
 void pageRank(double graph[N][N], double rank[N]) {
     double newRank[N]; 
     int iter = 0;
 
-    // Initialize ranks to 1/N
     for (int i = 0; i < N; i++) {
         rank[i] = 1.0 / N;
     }
 
-    // Iterate to update ranks
     while (iter < MAX_ITER) {
         for (int i = 0; i < N; i++) {
-            newRank[i] = (1.0 - DAMPING) / N; // Basic rank formula (damping factor)
+            newRank[i] = (1.0 - DAMPING) / N;
             
-            // Sum the contribution from other nodes
             for (int j = 0; j < N; j++) {
-                if (graph[j][i] != 0) { // There's a link from product j to product i
+                if (graph[j][i] != 0) {
                     int outDegree = 0;
-                    // Calculate out-degree of node j
                     for (int k = 0; k < N; k++) {
                         if (graph[j][k] != 0) {
                             outDegree++;
@@ -36,7 +31,6 @@ void pageRank(double graph[N][N], double rank[N]) {
             }
         }
 
-        // Update ranks
         for (int i = 0; i < N; i++) {
             rank[i] = newRank[i];
         }
@@ -45,7 +39,6 @@ void pageRank(double graph[N][N], double rank[N]) {
     }
 }
 
-// Function to print the product ranks
 void printRanks(double rank[N]) {
     printf("Product Ranks:\n");
     for (int i = 0; i < N; i++) {
@@ -54,22 +47,17 @@ void printRanks(double rank[N]) {
 }
 
 int main() {
-    // Graph representing product co-purchases
-    // graph[i][j] = 1 means there is a link from product i to product j (i.e., co-purchased)
     double graph[N][N] = {
-        {0, 1, 0, 0}, // Product 1 -> Product 2
-        {0, 0, 1, 0}, // Product 2 -> Product 3
-        {0, 0, 0, 1}, // Product 3 -> Product 4
-        {1, 0, 0, 0}  // Product 4 -> Product 1
+        {0, 1, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1},
+        {1, 0, 0, 0}
     };
 
-    // Array to store the ranks of products
     double rank[N];
 
-    // Calculate PageRank
     pageRank(graph, rank);
 
-    // Print the ranks of products
     printRanks(rank);
 
     return 0;
